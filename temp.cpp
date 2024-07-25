6e8b447f08b7570eeb0ea35e0ad5428e2bd66c0c
Naranbat <massive0304@gmail.com>
2024-07-26 07:28:07 +0800

    feat: temp file

--- temp.cpp
+++ temp.cpp
@@ -1,13 +1,34 @@
-#include<iostream>
+#include<bits/stdc++.h>
+
+#define ll long long
+#define F first
+#define S second
+#define mp make_pair
+
+
+ll gcd(ll a, ll b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+ 
+    if (a == b)
+        return a;
+ 
+    if (a > b)
+        return gcd(a - b, b);
+    return gcd(a, b - a);
+}
 
 using namespace std;
 
 int main() {
 
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     int t;
     cin >> t;
-
-    
 
 }
 
