0a1d781e920fff35ca1d9c6f78237d16ab8316e5
Naranbat <massive0304@gmail.com>
2025-04-23 03:47:34 +0800

    feat: CSES

--- CSES1673.cpp
+++ CSES1673.cpp
@@ -6,6 +6,9 @@
 #define mp make_pair
 #define pb push_back
 
+using namespace std;
+
+
 const ll MOD = 1e9 + 7;
 ll gcd(ll a, ll b) {
     if (b == 0)
@@ -13,47 +16,58 @@ ll gcd(ll a, ll b) {
     return gcd(b, a % b);
 }
 
-using namespace std;
+
+const long long INF = LONG_MAX;
+
+struct Edge {
+    int from, to;
+    long long weight;
+};
 
 int main() {
 
     ios::sync_with_stdio(false);
     cin.tie(0);
+    
     int n, m;
     cin >> n >> m;
 
-    vector<int>ans(n, 0);
-
-    vector<vector<pair<int,int> > > adj(n);
-    vector<bool>vis(n, false);
+    vector<Edge> edges;
 
     for(int i = 0; i < m; i++) {
-        int a,b, c;
-        cin >> a >> b >> c;
-        a--;
-        b--;
-        adj[a].pb(mp(b,c));
-        adj[b].pb(mp(a,c));
+        int a, b;
+        ll x;
+        cin >> a >> b >> x;
+        edges.pb({a, b, x});
     }
 
-    queue<pair<int,int> > q;
-    q.push(mp(0,0));
-    while(!q.empty()) {
-        auto front = q.front();
-        q.pop();
-        vis[front.F] = true;
 
-        for(auto node: adj[front.F]) {
-            if(!vis[node.F]) {
-                int maxPrice = max(ans[front.F] + front.S, ans[node.F]);
-                ans[node.F] = maxPrice;
-                q.push(mp(node.F, maxPrice));
-            }
+    vector<ll> dist(n + 1, -INF);
+    dist[1] = 0;
+
+    for(int i = 0; i < n - 1; i++) {
+        for(auto e: edges) {
+            if(dist[e.from] != -INF)
+                dist[e.to] = max(dist[e.to], dist[e.from] + e.weight);
         }
     }
-    for(int i = 0; i < n; i++)  
-        cout << ans[i] << ' ';
-    cout << ans[n];
+
+    vector<bool> in_cycle(n + 1, false);
+
+    for (int i = 0; i < n; i++) {
+        for (auto e : edges) {
+            if (dist[e.from] == -INF) continue;
+            if (dist[e.from] + e.weight > dist[e.to]) {
+                dist[e.to] = INF;
+                in_cycle[e.to] = true;
+            }
+            if (in_cycle[e.from])
+                in_cycle[e.to] = true;
+        }
+    }
+
+    if (in_cycle[n]) cout << -1 << '\n';
+    else cout << dist[n] << '\n';
 
     return 0;
 
@@ -62,5 +76,4 @@ int main() {
 /*
 RILY <3
 NEVER GIVE UP!!!
-5
 */
