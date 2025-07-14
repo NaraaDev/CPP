7696cbf20d2889a773896373bb0d4c5bbba26b42
Naranbat <massive0304@gmail.com>
2025-07-14 23:30:28 +0800

    feat: 1081.cpp

---
+++ CSES1081.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+
+
+#define ll long long
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+const ll MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+void uCan() {
+
+    int n;
+    cin >> n;
+
+    vector<ll> a(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    
+
+    
+    
+    ll maxVal = *max_element(a.begin(), a.end());
+    vector<ll> count(maxVal + 1, 0);
+    for(int i = 0; i < n; i++)
+        count[a[i]]++;
+
+    for(int i = maxVal; i >= 1; i--) {
+        int check = 0;
+        for(int j = i; j <= maxVal; j += i) {
+            check += count[j];
+            if(check >= 2) {
+                cout << i << '\n';
+                return;
+            }
+        }
+    }
+
+
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    // cin >> t;
+    t = 1;
+    while(t--) {
+        uCan();
+    }
+
+    return 0;
+
+}
+
+/*
+N - ee shalga
+Bitgii buuj ug
+Сая сая мөрөөдөл минь удахгүй нэг нэгээрэ биелэж эхлэх болно.
+Whatever our souls are made of, hers and mine are the same.
+People aren’t prisoners of fate, they’re prisoners of their own minds.
+*/
