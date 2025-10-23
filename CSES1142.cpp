d8f02503a67959c27bd575900c4fec4568f3f6c5
Naranbat <massive0304@gmail.com>
2025-10-24 00:32:25 +0800

    feat: cses

---
+++ CSES1142.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+
+
+#define ll long long
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+const ll MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll getArea(const vector<ll>& a, ll h) {
+    ll n = a.size();
+    ll cnt = 0, best = 0;
+    for (ll i = 0; i < n; i++) {
+        if (a[i] >= h) {
+            cnt++;
+            best = max(best, cnt * 1LL * h);
+        } else cnt = 0;
+    }
+    return best;
+}
+
+void uCan() {
+
+    int n;
+    cin >> n;
+
+    vector<ll> nums(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    ll ans = 0;
+
+    stack<ll> st;
+
+    int i = 0;
+
+    while(i < n) {
+        if(st.empty() || nums[st.top()] <= nums[i]) {
+            st.push(i++);
+        } else {
+            int tp = st.top();
+            st.pop();
+            ll width = st.empty() ? i : i - st.top() - 1;
+            ans = max(ans, nums[tp] * width);
+        }
+    }
+
+    while(!st.empty()) {
+        int tp = st.top();
+        st.pop();
+        ll width = st.empty() ? i : i - st.top() - 1;
+        ans = max(ans, nums[tp] * width);
+    }
+
+    cout << ans;
+
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    // cin >> t;
+    t = 1;
+    while(t--) {
+        uCan();
+    }
+
+    return 0;
+
+}
+
+/*
+N - ee shalga
+Bitgii buuj ug
+Сая сая мөрөөдөл минь удахгүй нэг нэгээрэ биелэж эхлэх болно.
+Whatever our souls are made of, hers and mine are the same.
+People aren’t prisoners of fate, they’re prisoners of their own minds.
+Whatever happened, Whatever was, Whatever you endured,
+Whatever changed — You can do it, you will improve.
+*/
