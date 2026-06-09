eb603f23744ffefa4d66888ba1950fd557b6aba3
naranbat <NaraaDev>
2026-06-09 17:45:51 +0800

    feat: CSES 1083

---
+++ CSES1083.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+
+
+#define ll long long
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll binpow(ll a, ll b) {
+
+    ll res = 1;
+
+    while(b > 0) {
+        if(b & 1) {
+            res = res * a;
+        }
+        a = a * a;
+        b /= 2;
+    }
+    return res;
+}
+
+void solve() {
+
+    ll n;
+    cin >> n;
+
+    ll sum = n * (n + 1) / 2;
+    ll p;
+    for(int i = 0; i < n - 1; i++) {
+        cin >> p;
+        sum -= p;
+    }
+
+    cout << sum;
+
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+
+}
+
+/*
+N - ee shalga
+Bitgii buuj ug
+Сая сая мөрөөдөл минь удахгүй нэг нэгээрэ биелэж эхлэх болно.
+Whatever happened, Whatever was, Whatever you endured,
+Whatever changed — You can do it, you will improve.
+*/
