9080c63093c1f68fc14cd693225e138d8b15bed3
naranbat <NaraaDev>
2026-06-24 15:32:42 +0800

    feat: llll

---
+++ 1418A.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+
+
+#define ll long long
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll binpow(ll a, ll b) {
+
+    ll res = 1;
+
+    while(b > 0) {
+        if(b & 1) {
+            res = res * a;
+        }
+        a = a * a;
+        b /= 2;
+    }
+    return res;
+}
+
+void solve() {
+
+    ll x,y,k;
+    cin >> x >> y >> k;
+
+    ll ans = k;
+
+    ll allNeedStick = y * k + k;
+
+    ans += (allNeedStick - 1) / (x - 1) + ((allNeedStick - 1) % (x - 1) != 0);
+
+    cout << ans << '\n';
+
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+
+}
+
+/*
+N - ee shalga
+Bitgii buuj ug
+Сая сая мөрөөдөл минь удахгүй нэг нэгээрэ биелэж эхлэх болно.
+Whatever happened, Whatever was, Whatever you endured,
+Whatever changed — You can do it, you will improve.
+*/
