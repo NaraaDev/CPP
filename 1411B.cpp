6a24e84f7dc5d2349753a66bbe0969cecddb2620
naranbat.m <naranbat.m@Naranbats-MacBook-Pro.local>
2026-09-08 17:42:03 +0800

    feat: problem

---
+++ 1411B.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll binpow(ll a, ll b)
+{
+
+    ll res = 1;
+
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = res * a;
+        }
+        a = a * a;
+        b /= 2;
+    }
+    return res;
+}
+
+bool check(ll n, vector<int> &digits)
+{
+    for (int i = 0; i < digits.size(); i++)
+    {
+        if (n % digits[i] != 0)
+            return false;
+    }
+    return true;
+}
+
+void solve()
+{
+
+    ll n;
+    cin >> n;
+
+    if (n <= 10)
+    {
+        cout << n << '\n';
+        return;
+    }
+
+    ll cn = n;
+    while (cn)
+    {
+        ll d = cn % 10;
+        if (d == 0 || n % d == 0)
+        {
+            cn /= 10;
+        }
+        else
+        {
+            n++;
+            cn = n;
+        }
+    }
+
+    cout << n << '\n';
+}
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+/*
+N - ee shalga
+Bitgii buuj ug
+Сая сая мөрөөдөл минь удахгүй нэг нэгээрэ биелэж эхлэх болно.
+Whatever happened, Whatever was, Whatever you endured,
+Whatever changed — You can do it, you will improve.
+*/
