22f775a41d5382288fc6b04dea3128ba7d001fb1
naranbat.m <naranbat.m@Naranbats-MacBook-Pro.local>
2026-09-22 17:04:00 +0800

    feat: CSES

---
+++ CSES1618.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll binpow(ll a, ll b)
+{
+
+    ll res = 1;
+
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = res * a;
+        }
+        a = a * a;
+        b /= 2;
+    }
+    return res;
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    int ans = 0;
+
+    while (n > 0)
+    {
+        n /= 5;
+        ans += n;
+    }
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+/*
+N - ee shalga
+Bitgii buuj ug
+Сая сая мөрөөдөл минь удахгүй нэг нэгээрэ биелэж эхлэх болно.
+Whatever happened, Whatever was, Whatever you endured,
+Whatever changed — You can do it, you will improve.
+*/
